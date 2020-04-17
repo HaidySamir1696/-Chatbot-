2ede57f74a18fb5506a607c5b35a0242e783ea24
AbdoWahba <abdo.tw20154@gmail.com>
2020-04-17 15:21:16 +0200

    get squad like json

--- parser/fineTuning/squad1.1-like.py
+++ parser/fineTuning/squad1.1-like.py
@@ -8,13 +8,24 @@ with open('./cache/dfjson.json', 'r') as f:
 
 squadLike = {"data": [{"title":dfjson["title"]["0"], "paragraphs":[]}]}
 paragraphs = []
+teamPar=[]
 
 for paragraph in dfjson["paragraphs"]["0"]:
     paragraphs.append({"context":paragraph,"qas":[]})
+
+teamNo = int(input("enter number of collaborators:"))
+
+for i in range(0, teamNo):
+    teamPar = dict(squadLike)
+    teamPar["data"][0]["paragraphs"] = list(paragraphs[int((i/teamNo)*len(paragraphs)):int(((i+1)/teamNo)*len(paragraphs))])
+    with open('./cache/cdqa'+str(dfjson["title"]["0"])+str(i+1)+'.json', 'w') as f:
+        f.write(str(squadLike))
 
 squadLike["data"][0]["paragraphs"] = list(paragraphs)
 
 with open('./cache/cdqa'+str(dfjson["title"]["0"])+'.json', 'w') as f:
     f.write(str(squadLike))
+
+# dividing files
 
 
